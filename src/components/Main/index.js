720c950696e7c24868983bc4699d37e0fb046f7e
Marlon Matthew <mbm.drums@gmail.com>
2021-04-24 17:50:13 -0700

    added basic main component

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Data from "../Data/index.js";
+import "./Main.css";
+
+function Main() {
+  return (
+    <>
+      <Data />
+    </>
+  );
+}
+
+export default Main;
