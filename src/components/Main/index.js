e450640114b7a4cb939bfe929cf1cac708df8442
Marlon Matthew <mbm.drums@gmail.com>
2021-04-24 17:53:44 -0700

    added basic nav component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Data from "../Data/index.js";
-import "./Main.css";
 
 function Main() {
   return (
---
+++ src/components/Nav/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./Nav.css";
+
+function Nav() {
+  return <nav className="navbar navbar-expand-lg navbar-light bg-light"></nav>;
+}
+export default Nav;
